7194eb2099064e77f84de450ffa3725508d9321a
unknown <toyenphuong10x@gmail.com>
2023-03-17 21:18:08 -0700

    third try

--- Develop/script.js
+++ Develop/script.js
@@ -1,23 +1,56 @@
 // Wrap all code that interacts with the DOM in a call to jQuery to ensure that
 // the code isn't run until the browser has finished rendering all the elements
 // in the html.
+const saveButton = $('.saveBtn');
+const input = $('textarea');
+const schedule = $('.container-lg .time-block');
+
+
 $(function () {
-  // TODO: Add a listener for click events on the save button. This code should
-  // use the id in the containing time-block as a key to save the user input in
-  // local storage. HINT: What does `this` reference in the click listener
-  // function? How can DOM traversal be used to get the "hour-x" id of the
-  // time-block containing the button that was clicked? How might the id be
-  // useful when saving the description in local storage?
-  //
-  // TODO: Add code to apply the past, present, or future class to each time
-  // block by comparing the id to the current hour. HINTS: How can the id
-  // attribute of each time-block be used to conditionally add or remove the
-  // past, present, and future classes? How can Day.js be used to get the
-  // current hour in 24-hour time?
-  //
-  // TODO: Add code to get any user input that was saved in localStorage and set
-  // the values of the corresponding textarea elements. HINT: How can the id
-  // attribute of each time-block be used to do this?
-  //
-  // TODO: Add code to display the current date in the header of the page.
+
+  var currentDate = new Date($.now());
+  var currentHour = currentDate.getHours();
+  var daysOfWeek = ['Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday'];
+  var monthsOfYear = ['January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December'];
+  var dayOfWeek = daysOfWeek[currentDate.getDay()];
+  var dayOfMonth = currentDate.getDate();
+  var monthOfYear = monthsOfYear[currentDate.getMonth()];
+  var dateString = dayOfWeek + ', ' + monthOfYear + ' ' + dayOfMonth;
+
+  $('#currentDay').text(dateString);
+  for (var i = 0; i < schedule.length; i++) {
+    if (i + 9 < currentHour) {
+      $(schedule[i]).addClass('past');
+    } 
+    else if (i + 9 === currentHour) {
+      $(schedule[i]).addClass('present');
+    }
+    else $(schedule[i]).addClass('future');
+    console.log(i + 9, "current: ", currentHour);
+  }
+
+  $(document).ready(function() {
+    // Load saved values from local storage and display them on page load
+    
+    $('.description').each(function() {
+      console.log($(this).val());
+      var key = $(this).data('key');
+      var savedValue = localStorage.getItem(key);
+      if (savedValue) {
+        $(this).val(savedValue);
+      }
+    });
+  
+    // Add click event listener to each save button
+    $('.saveBtn').click(function() {
+      // Find the text input element within the same container element
+      var input = $(this).closest('.time-block').find('.description');
+      var inputValue = input.val();
+  
+      // Save the input value to local storage
+      var key = input.data('key');
+      localStorage.setItem(key, inputValue);
+    });
+  });
+
 });
